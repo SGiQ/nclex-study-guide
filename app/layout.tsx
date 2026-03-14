03171aeac9b1b202715ac9ee2185cb8a495f1392
SGiQ <sgiqbusiness@gmail.com>
2026-03-14 18:48:30 -0400

    fix: restore Material Symbols icons and resolve 400 font loading error

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,8 +50,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@100..700,0..1&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-25..0&display=block" rel="stylesheet" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
